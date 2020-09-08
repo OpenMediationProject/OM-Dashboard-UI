5a9169d287a31a0e1cabc8044cb7cb01bd652472
huangqiang <huangqiang@1tu1shu.com>
2020-09-08 10:37:58 +0800

    1.Removes the WangEditor component 2.Placement selection component increases App linkage

--- src/components/om/PlacementSelectSimple.vue
+++ src/components/om/PlacementSelectSimple.vue
@@ -8,12 +8,13 @@
       mode="multiple"
       :disabled="disabled"
       :maxTagCount="1"
-      :showArrow="false"
-      :maxTagTextLength="10"
+      :maxTagTextLength="7"
       :maxTagPlaceholder="(h,vm) => '+' + (vm.value.length - 1)"
       allowClear
+      :showArrow="false"
       optionLabelProp="title"
       :filterOption="filterOption"
+      :dropdownMatchSelectWidth="false"
       @change="handleChange"
       v-decorator="[name, {initialValue: initValue}]">
       <a-select-option v-for="o in this.plcList" :key="o.id" :title="o.name">
@@ -54,9 +55,17 @@ export default {
         return []
       }
     },
+    pubAppIds: {
+      type: Array,
+      default: null
+    },
     disabled: {
       type: Boolean,
       default: false
+    },
+    form: {
+      type: Object,
+      default: null
     }
   },
   data () {
@@ -65,18 +74,54 @@ export default {
       plcList: {}
     }
   },
+  watch: {
+    pubAppIds (val) {
+      this.resetOptions()
+    },
+    defaultValue (v) {
+      this.initValue = v
+      this.$emit('change', v)
+    }
+  },
   methods: {
+    resetOptions () {
+      const that = this
+      if (this.pubAppIds && this.pubAppIds.length) {
+        placementSelectList({ pubAppIds: this.pubAppIds.join(',') }).then(res => {
+          this.plcList = res.data
+          if (that.initValue && that.initValue.length) {
+            const newArray = res.data.map(obj => { return obj.id })
+            that.initValue = that.initValue.filter(item => {
+              return newArray.includes(item)
+            })
+            that.form.setFieldsValue({ placementId: that.initValue })
+          }
+        })
+      } else {
+        placementSelectList().then(res => {
+          this.plcList = res.data
+        })
+      }
+    },
     handleChange (value) {
+      this.initValue = value
       this.$emit('change', value)
     },
     filterOption (input, option) {
       return (option.componentOptions.children[0].children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0)
     }
   },
-  async created () {
+  created () {
     try {
-      const res = await placementSelectList()
-      this.plcList = res.data
+      const params = {}
+      if (this.pubAppIds && this.pubAppIds.length) {
+        params.pubAppIds = this.pubAppIds.join(',')
+      }
+      placementSelectList(params).then(res => {
+        if (res.data) {
+          this.plcList = res.data
+        }
+      })
     } catch (e) {
       console.log('load placement list error', e)
     }
